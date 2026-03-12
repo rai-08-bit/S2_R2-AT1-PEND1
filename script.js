50fd2f45fa719042cf763e7cff878f246846b73d
RAÍSSA PEREIRA DA SILVA BEZERRA <raissa.bez08@outlook.com>
2026-03-12 13:11:12 -0300

    At3 Finalizada

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-let btnEnviar = document.querySelector("#btnEnviar");
+let btnAdicionar = document.querySelector("#btnAdicionar");
+let inputTarefa = document.querySelector("#inputTarefa");
 let mensagem = document.querySelector("#mensagem");
+let listaTarefas = document.querySelector("#listaTarefas");
 
-btnEnviar.addEventListener("click", () => {
-    mensagem.innerText = `CABUMMMMMMM! Texto modificado com Java Script`;
-    mensagem.className = "text-success-emphasis bg-success-subtle border border-success-subtle rounded-3"
+btnAdicionar.addEventListener("click", () => {
+    let InputTarefa = inputTarefa.value;
+    if (InputTarefa.trim() === "") {
+        mensagem.innerText = `Por favor, insira uma tarefa.`
+        mensagem.className = "p-3 text-center text-danger-emphasis bg-danger-subtle border border-danger-subtle rounded-3"
+        return
+    }
+
+    mensagem.innerText = `A tarefa "${InputTarefa}" foi adicionada com sucesso!`
+    mensagem.className = "p-3 text-center text-info-emphasis bg-info-subtle border border-info-subtle rounded-3"
+
+    let novaTarefa = document.createElement("li");
+    novaTarefa.innerText = InputTarefa;
+    novaTarefa.className = "list-group-item";
+    listaTarefas.appendChild(novaTarefa);
 })
